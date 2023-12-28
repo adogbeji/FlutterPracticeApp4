dc93717d0e0b978976bcf453dbd47c717b50fe61
adogbeji <baka_1993@yahoo.co.uk>
2023-12-28 18:17:36 +0000

    Start creating InputTextWidget

---
+++ lib/widgets/input_text_widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class InputTextWidget extends StatelessWidget {
+  final TextEditingController textEditingController;
+  final IconData? iconData;
+
+  const InputTextWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
