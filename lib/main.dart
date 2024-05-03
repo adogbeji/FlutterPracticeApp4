b0f1442096e1254732968be45cb681b9837d7e76
adogbeji <baka_1993@yahoo.co.uk>
2024-05-03 13:48:28 +0100

    Set dark theme

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      theme: ThemeData.dark().copyWith(
+        scaffoldBackgroundColor: Colors.black,
       ),
       home: const LoginScreen(),
     );
