3bfe16b489bc5571d039be580c4875d5afad744a
adogbeji <baka_1993@yahoo.co.uk>
2024-05-13 13:49:31 +0100

    Create InputTextWidget & add properties

---
+++ lib/widgets/input_text_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class InputTextWidget extends StatelessWidget {
+  final TextEditingController textEditingController;
+  final IconData? iconData;
+  final String? assetReference;
+  final String labelString;
+  final bool isObscure;
+
+  const InputTextWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
